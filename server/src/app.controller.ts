7a1f69d2cc9e9003014b5441e804b9f995ee8310
ionextdebug <focusmpublic@gmail.com>
2024-01-19 12:46:04 -0400

    feat(server): integrating i18n with NestJS

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { I18n, I18nContext } from 'nestjs-i18n';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getHello() {
-    return this.appService.getHello(1);
+  getHello(@I18n() i18n: I18nContext) {
+    return  i18n.t('test.HELLO');
+    //return this.appService.getHello(1);
   }
 }
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import {I18nModule} from 'nestjs-i18n';
+import * as path from 'path';
 
+
+console.log(path.join(__dirname, '/i18n/'))
 @Module({
-  imports: [],
+  imports: [I18nModule.forRoot({
+    fallbackLanguage: 'en',
+    loaderOptions: {
+      path: path.join(__dirname, '/i18n/'),
+      watch: true,
+    },
+  })],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
