fab8abd2b8f40b37f67482352be3522b7ce22297
ionextdebug <focusmpublic@gmail.com>
2024-01-19 18:48:51 -0400

    feat(server): testing endpoints answers

--- server/src/system-user/system-user.controller.spec.ts
+++ server/src/system-user/system-user.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SystemUserController } from './system-user.controller';
 import { SystemUserService } from './system-user.service';
+import { APIAnswer } from 'src/types';
 
 describe('SystemUserController', () => {
   let controller: SystemUserController;
@@ -17,4 +18,69 @@ describe('SystemUserController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('These methods should be defined', ()=>{
+
+
+    it('create should be defined', ()=>{
+      expect(controller.create).toBeDefined();
+    })
+    
+    it('findAll should be defined', ()=>{
+      expect(controller.findAll).toBeDefined();
+    })
+
+    it('findOne should be defined', ()=>{
+      expect(controller.findOne).toBeDefined();
+    })
+
+    it('update should be defined', ()=>{
+      expect(controller.update).toBeDefined();
+    })
+
+    it('remove should be defined', ()=>{
+      expect(controller.remove).toBeDefined();
+    })
+
+    it('auth should be defined', ()=>{
+      expect(controller.remove).toBeDefined();
+    })
+
+  })
+
+  describe('These methods should return an object of APIAnswer type.', ()=>{
+
+    it('create should be of type APIAnswer', ()=>{
+      const answer : APIAnswer = controller.create({});
+      
+    })
+    
+    it('findAll should be of type APIAnswer', ()=>{
+      const answer : APIAnswer = controller.findAll();
+      
+    })
+
+    it('findOne should be of type APIAnswer', ()=>{
+      const answer : APIAnswer = controller.findOne('1');
+      
+    })
+
+    it('update should be of type APIAnswer', ()=>{
+      const answer : APIAnswer = controller.update('1', {});
+      
+    })
+
+    it('remove should be of type APIAnswer', ()=>{
+      const answer : APIAnswer = controller.remove('1');
+      
+    })
+
+    it('auth should be of type APIAnswer', ()=>{
+      const answer : APIAnswer = controller.auth();
+      
+    })
+
+
+  })
+
 });
--- server/src/system-user/system-user.controller.ts
+++ server/src/system-user/system-user.controller.ts
@@ -2,33 +2,46 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { SystemUserService } from './system-user.service';
 import { CreateSystemUserDto } from './dto/create-system-user.dto';
 import { UpdateSystemUserDto } from './dto/update-system-user.dto';
+import { APIAnswer } from 'src/types';
 
 @Controller('system-user')
 export class SystemUserController {
   constructor(private readonly systemUserService: SystemUserService) {}
 
   @Post()
-  create(@Body() createSystemUserDto: CreateSystemUserDto) {
-    return this.systemUserService.create(createSystemUserDto);
+  create(@Body() createSystemUserDto: CreateSystemUserDto) : APIAnswer {
+    const answer = {success: false, description: "", data: []};
+    return answer;
   }
 
   @Get()
-  findAll() {
-    return this.systemUserService.findAll();
+  findAll() : APIAnswer  {
+    const answer = {success: false, description: "", data: []};
+    return answer;
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.systemUserService.findOne(+id);
+  findOne(@Param('id') id: string) : APIAnswer  {
+    const answer = {success: false, description: "", data: []};
+    return answer;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSystemUserDto: UpdateSystemUserDto) {
-    return this.systemUserService.update(+id, updateSystemUserDto);
+  update(@Param('id') id: string, @Body() updateSystemUserDto: UpdateSystemUserDto) : APIAnswer  {
+    const answer = {success: false, description: "", data: []};
+    return answer;
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.systemUserService.remove(+id);
+  remove(@Param('id') id: string) : APIAnswer  {
+    const answer = {success: false, description: "", data: []};
+    return answer;
   }
+
+  @Post()
+  auth() : APIAnswer  {
+    const answer = {success: false, description: "", data: []};
+    return answer;
+  }
+
 }
